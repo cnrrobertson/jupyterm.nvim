cdaaaeec6cf11f03ee3d8dd7bfc4c0d389d7f0fc
Connor Robertson <cnr.robertson@gmail.com>
2024-12-03 12:10:43 -0600

    bugfix: shutdown for kernels whose windows haven't been shown

--- lua/jupyterm/manage_kernels.lua
+++ lua/jupyterm/manage_kernels.lua
@@ -34,7 +34,9 @@ function manage_kernels.shutdown_kernel(kernel)
   if kernel == nil then
     kernel = utils.get_kernel_buf_or_buf()
   end
-  vim.api.nvim_buf_delete(Jupyterm.kernels[kernel].show_win.bufnr, {force=true})
+  if Jupyterm.kernels[kernel].show_win then
+    vim.api.nvim_buf_delete(Jupyterm.kernels[kernel].show_win.bufnr, {force=true})
+  end
   Jupyterm.kernels[kernel] = nil
   vim.fn.JupyShutdown(tostring(kernel))
 end
