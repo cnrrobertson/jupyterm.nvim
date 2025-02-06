958ae3184c9c2c413fa513a59ca267c61060ed70
Connor Robertson <cnr.robertson@gmail.com>
2025-02-06 12:57:15 -0600

    update: add descriptions in the config file

--- lua/jupyterm/config.lua
+++ lua/jupyterm/config.lua
@@ -7,15 +7,22 @@
 ---@field output_refresh table
 ---@field ui table
 local config = {
+  -- The default Jupyter kernel to use
   default_kernel = "python3",
+  -- Focus the REPL window when showing
   focus_on_show = true,
+  -- Show the REPL window when sending text to it
   show_on_send = false,
+  -- Focus the REPL window when sending text to it
   focus_on_send = false,
+  -- Show outputs inline as virtual text when sending text
   inline_display = true,
+  -- Automatic refreshing of REPL window and virtual text
   output_refresh = {
     enabled = true,
     delay = 2000,
   },
+  -- UI options for the REPL window and the menu
   ui = {
     format = "split",
     config = {
@@ -44,6 +51,5 @@ local config = {
     }
   }
 }
-
 
 return config
